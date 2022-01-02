cf6e06d258810cb9566ad55115dd07137e3c36f3
Shivay Sharma <sharmashivay37@gmail.com>
2022-01-03 00:44:46 +0530

    Uploading app

---
+++ app/src/main/java/com/shivay/ChatApplication/SignIn.kt
@@ -0,0 +1,67 @@
+package com.shivay.ChatApplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+
+class SignIn : AppCompatActivity() {
+
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var edtName: EditText
+    private lateinit var btnSignIn: Button
+    private lateinit var mDbRef: DatabaseReference
+
+    private lateinit var mAuth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+        supportActionBar?.hide()
+        mAuth = FirebaseAuth.getInstance()
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        edtName = findViewById(R.id.edt_name)
+        btnSignIn = findViewById(R.id.btnSignin)
+
+        btnSignIn.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val name = edtName.text.toString()
+            val password = edtPassword.text.toString()
+
+            signin(name,email,password)
+        }
+    }
+
+    private fun signin(name:String ,email: String,password: String)
+    {
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                   addUserToDatabase(name,email,mAuth.currentUser?.uid!!)
+                    val intent = Intent(this@SignIn,MainActivity::class.java)
+                    finish()
+                    startActivity(intent)
+                }
+                else {
+                    Toast.makeText(this@SignIn,"Error Occurred",Toast.LENGTH_SHORT).show()
+
+                }
+            }
+    }
+    private fun addUserToDatabase(name: String,email: String,uid:String){
+        mDbRef = FirebaseDatabase.getInstance().getReference()
+
+        mDbRef.child("user").child(uid).setValue(User(name,email,uid))
+
+
+
+    }
+
+    }
